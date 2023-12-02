88d008a667ff15269e93cecd36dd3fae531ede27
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-12-02 09:17:20 +0000

    [Finish]応用課題3　いいね&コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "/home/about" => "homes#about", as:"about"
-  resources :books, only: [:new,:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:new,:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only:[:create, :destroy]
+    resource :favorite, only:[:create, :destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
