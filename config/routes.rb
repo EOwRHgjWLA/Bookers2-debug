c539649e30e7f1c9011e391889b63601b1ad5d0a
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-12-07 09:18:13 +0000

    [Finish]応用課題6 いいね/コメントの非同期通信化

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   get "/home/about" => "homes#about", as:"about"
   get "/search", to: "searches#search"
 
-  resources :books, only: [:new,:index,:show,:edit,:create,:destroy,:update] do
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resources :book_comments, only:[:create, :destroy]
-    resource :favorite, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
   end
 
-  resources :users, only: [:index,:show,:edit,:update] do
+  resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only:[:create, :destroy]
       get "followings" => "relationships#followings", as:"followings"
       get "followers" => "relationships#followers", as:"followers"
